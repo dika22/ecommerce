1a0e7f8b91fd7882b9d9f407448f2df6b1bb9454
Adhika pamungkas <adhikapamungkas22@gmail.com>
2025-07-11 18:13:18 +0700

    comment rabbitMQ

--- warehouse-service/main.go
+++ warehouse-service/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 	"warehouse-service/internal/domain/stock/repository"
 	"warehouse-service/internal/domain/stock/usecase"
 	"warehouse-service/package/config"
 	"warehouse-service/package/connection/database"
-	rabbitmq "warehouse-service/package/rabbit-mq"
 
 	repoWh "warehouse-service/internal/domain/warehouse/repository"
 	uw "warehouse-service/internal/domain/warehouse/usecase"
@@ -22,16 +19,16 @@ import (
 func main() {
 
   dbConf := config.NewDatabase()
-  conf := config.NewConfig()
+  // conf := config.NewConfig()
   conn := database.WebDB
   dbConn := database.NewDatabase(conn, dbConf)
 
-  mqClient, err := rabbitmq.NewRabbitMQClient(conf)
-  if err != nil {
-    log.Println("ERROR INIT RABBITMQ", err)
-  }
+  // mqClient, err := rabbitmq.NewRabbitMQClient(conf)
+  // if err != nil {
+  //   log.Println("ERROR INIT RABBITMQ", err)
+  // }
 
-  fmt.Println("DEBUG", mqClient)
+  // fmt.Println("DEBUG", mqClient)
 
 
   repo := repository.NewRepositoryStock(dbConn)
